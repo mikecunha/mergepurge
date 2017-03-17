994073cea36524f6014d4e47324bb006a85408df
Mike Cunha <mikecunha@gmail.com>
2017-03-17 09:59:36 -0400

    Parametrized tests of clean module
    Functions in the clean module were previously tested by reparsing a
    batch of 100 test cases at a time, if a single test case failed the
    whole test would fail. By parametrizing the test functions each test
    case in the test data will be treated as an individual test.

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -1,3 +1,4 @@
+import pytest
 from mergepurge import clean
 import pandas as pd
 import numpy as np
@@ -16,30 +17,31 @@ partial = pd.read_csv('./incomplete.tsv',
                       sep='\t', encoding='utf-8',
                       dtype={'aa_streetnum': str, 'aa_zip': str, 'zipcode': str})
 
-
-def test_clean_contact_name():
-    # Re-parse approx 100 records
-    for _, test_record in complete.iterrows():
-        known = (test_record.get('aa_title', np.nan), test_record.get('aa_firstname', np.nan),
-                 test_record.get('aa_lastname', np.nan), test_record.get('aa_fullname', np.nan))
-        parsed = clean.parse_contact_name(test_record, COMP_CONTACT_COLS, strict=False)
-        assert parsed == known
+trecords = [row for (index, row) in complete.iterrows()]
 
 
-def test_parse_location_cols():
-    for _, test_record in complete.iterrows():
-        known = (test_record.get('aa_streetnum', np.nan), test_record.get('aa_street', np.nan),
-                 test_record.get('aa_city', np.nan), test_record.get('aa_state', np.nan),
-                 test_record.get('aa_zip', np.nan), test_record.get('aa_fulladdy', np.nan))
-        parsed = clean.parse_location_cols(test_record, COMP_LOC_COLS, strict=False)
-        assert parsed == known
+@pytest.mark.parametrize('test_record', trecords)
+def test_clean_contact_name(test_record):
+    known = (test_record.get('aa_title', np.nan), test_record.get('aa_firstname', np.nan),
+             test_record.get('aa_lastname', np.nan), test_record.get('aa_fullname', np.nan))
+    parsed = clean.parse_contact_name(test_record, COMP_CONTACT_COLS, strict=False)
+    assert parsed == known
 
 
-def test_parse_business_name():
-    for _, test_record in complete.iterrows():
-        known = test_record.get('aa_company', np.nan)
-        parsed = clean.parse_business_name(test_record, COMP_COMPANY_COLS, strict=False)
-        assert parsed == known
+@pytest.mark.parametrize('test_record', trecords)
+def test_parse_location_cols(test_record):
+    known = (test_record.get('aa_streetnum', np.nan), test_record.get('aa_street', np.nan),
+             test_record.get('aa_city', np.nan), test_record.get('aa_state', np.nan),
+             test_record.get('aa_zip', np.nan), test_record.get('aa_fulladdy', np.nan))
+    parsed = clean.parse_location_cols(test_record, COMP_LOC_COLS, strict=False)
+    assert parsed == known
+
+
+@pytest.mark.parametrize('test_record', trecords)
+def test_parse_business_name(test_record):
+    known = test_record.get('aa_company', np.nan)
+    parsed = clean.parse_business_name(test_record, COMP_COMPANY_COLS, strict=False)
+    assert parsed == known
 
 
 def test_build_matching_cols():
